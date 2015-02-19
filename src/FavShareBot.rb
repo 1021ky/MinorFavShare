0157212da1461bed38b9bf7c1f154940ae474597
k_yamanaka <vaivailx+git@gmail.com>
2015-02-19 19:10:51 +0900

    ロガーを追加。クラスを再設計.

--- src/FavShareBot.rb
+++ src/FavShareBot.rb
@@ -2,11 +2,12 @@
 
 require 'twitter'
 require 'yaml'
-# リソースファイルの読み込み
-#load '../config/development.rb'
+require 'singleton'
+require 'logger'
 
-PARAMETER_FILENAME = "../config/development.yaml" # リソースファイル名
+LOG_FILE = "../log/FavShareBot.log"#ログ出力先
 
+# フォロワーのそこそこ人気のあるツイートをツイートする
 class FavShareBot
 	SELF_ID = "MinorFavShare"
 
@@ -14,22 +15,28 @@ class FavShareBot
 	MAX_POPULATION = 10
 	@client = nil
 
+	@logger = nil
+
 	public
+	def initialize
+		@logger = Logger.new(File.open(LOG_FILE,'a'), 'daily')
+		@logger.info "ini start"
+		@client = TwitterAuth.instance.client
+		@logger.debug @client.object_id
+		@logger.info "ini end"
+	end
 
 	# ツイートする
 	def tweet
 		begin
-			parameter = load_login_parameter()
-			login_twitter(parameter)
-			loop{
-				selected_follower = get_follower_random
-				favorite_tweet = get_minor_favorite(selected_follower)
-				if favorite_tweet != nil
-					tweet_text =  @client.user(selected_follower).name + " favorited " + favorite_tweet.uri
-					@client.update(tweet_text)
-				end
-				sleep 60 * 10
-			}
+			selected_follower = get_follower_random
+			favorite_tweet = get_minor_favorite(selected_follower)
+			if !(favorite_tweet.nil?)
+				tweet_text =  @client.user(selected_follower).name + " favorited " + favorite_tweet.uri
+				@client.update(tweet_text)
+				@logger.debug "tweet text is: " << tweet_text
+			end
+			sleep 60 * 10
 		rescue => ex
 			STDERR.puts ex.class.to_s << " raised in " << ex.backtrace[0].to_s
 		end
@@ -37,26 +44,54 @@ class FavShareBot
 
 	private
 
-	# リソースファイルからTwitterにログインするためのパラメータを読み込む
-	def load_login_parameter
-		parameters = YAML.load_file(PARAMETER_FILENAME)
-		return parameters["twitter"]
-	end
-
 	# フォロワーからランダムに一人を選ぶ
 	def get_follower_random
+		@logger.debug "pass random"
 		followers = @client.followers(SELF_ID)
-		followers.map.to_set.to_a.sample(1)
+		selected_follower = followers.map.to_set.to_a.sample(1)
+		@logger.debug "selected_follower:" << selected_follower
+		return selected_follower
 	end
 
 	# 指定されたユーザのお気に入りの中でまあまあの人気のものを取得
 	def get_minor_favorite(user)
+		@logger.debug "pass get_minor_favorite"
 		@client.favorites(user).to_set.each{|favorite|
 			popularity = favorite.favorite_count
 			if popularity >= MIN_POPULATION && popularity <= MAX_POPULATION
+				@logger.debug "minor_favorite:" << favorite << ", popularity:" << popularity
 				return favorite
 			end
 		}
+	end
+
+end
+
+# Twitterにアクセスするクライアントを管理するクラス
+class TwitterAuth
+	include Singleton
+
+	PARAMETER_FILENAME = "../config/development.yaml" # リソースファイル名
+
+	attr_accessor :client #Twitterのクライアント
+
+	@logger = nil
+
+	def initialize
+		@logger = Logger.new(File.open(LOG_FILE,'a'), 'daily')
+
+		parameter = load_login_parameter
+		login_twitter(parameter)
+	end
+
+	# リソースファイルからTwitterにログインするためのパラメータを読み込む
+	def load_login_parameter
+		parameters = YAML.load_file(PARAMETER_FILENAME)
+		@logger.debug "login parameter:" << parameters["twitter"]["consumer_key"]
+		@logger.debug "login parameter:" << parameters["twitter"]["consumer_secret"]
+		@logger.debug "login parameter:" << parameters["twitter"]["access_token"]
+		@logger.debug "login parameter:" << parameters["twitter"]["access_token_secret"]
+		return parameters["twitter"]
 	end
 
 	# Twitterにログインする
@@ -67,14 +102,18 @@ class FavShareBot
 			config.access_token        = parameter["access_token"]
 			config.access_token_secret = parameter["access_token_secret"]
 		end
+		@logger.debug @client.nil? ? "can¥'t login." : "logined."
 	end
 end
 
+# ボット用のUtilクラス
 class TwitterUtil
 
 	def format_tweet_within_140_text(tweet)
 
 	end
 end
+
+# 実行
 bot  = FavShareBot.new
 bot.tweet
